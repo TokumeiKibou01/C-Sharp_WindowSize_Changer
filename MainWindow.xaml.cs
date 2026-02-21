301671b1cc9782e94bfbb8b96c322f7b23543dd4
TokumeiKibou01 <snowsansub011307@gmail.com>
2026-02-21 21:07:59 +0900

    ＜追加＞　ウインドウの大きさを取得できるようになりました

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,22 @@ namespace C_Sharp_WindowSize_Changer {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        [DllImport("user32.dll")]
+        static extern bool MoveWindow(IntPtr hwnd, int x, int y, int width, int height, bool bRepaint);
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect); //ウインドウの大きさを取得する関数
+
+        /// <summary>
+        /// windef.h にある構造体
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT { 
+            public int Left; //ウインドウの左上X
+            public int Top; //ウインドウの左上Y
+            public int Right; //ウインドウの右下X
+            public int Bottom; //ウインドウの右下Y
+        }
+
         public MainWindow() {
             InitializeComponent();
             DataContext = new ViewModel();
@@ -23,6 +40,10 @@ namespace C_Sharp_WindowSize_Changer {
             foreach (var process in Process.GetProcesses()) {
                 if (process.MainWindowHandle != IntPtr.Zero) { // ウインドウハンドルがあるもののみ
                     viewModel.ProcessList.Add(process.ProcessName);
+                    if (GetWindowRect(process.MainWindowHandle, out RECT rect)) {
+                        viewModel.WindowWidth = rect.Right - rect.Left;
+                        break;
+                    }
                 }
             }
         }
